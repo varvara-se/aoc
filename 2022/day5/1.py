537784229fdee6143dbf8da6b34d78b318799ab3
Varvara Gioti <varvara.gioti@ingka.ikea.com>
2022-12-05 08:37:03 +0100

    feat: add day5 solution

---
+++ 2022/day5/1.py
@@ -0,0 +1,31 @@
+result = ''
+
+stacks = [['B', 'W', 'N'],
+          ['L', 'Z', 'S', 'P', 'T', 'D', 'M', 'B'],
+          ['Q', 'H', 'Z', 'W', 'R'],
+          ['W', 'D', 'V', 'J', 'Z', 'R'],
+          ['S', 'H', 'M', 'B'],
+          ['L', 'G', 'N', 'J', 'H', 'V', 'P', 'B'],
+          ['J', 'Q', 'Z', 'F', 'H', 'D', 'L', 'S'],
+          ['W', 'S', 'F', 'J', 'G', 'Q', 'B'],
+          ['Z', 'W', 'M', 'S', 'C', 'D', 'J']]
+
+with open('./input.txt') as f:
+    lines = [i.split('\n', 1)[0] for i in f.readlines()]
+
+for line in lines:
+    commands = line.split(',')
+    for command in commands:
+        words = command.split(' ')
+        amount = int(words[1])
+        from_stack = int(words[3])
+        to_stack = int(words[5])
+
+        for a in range(amount):
+            item = stacks[from_stack-1].pop()
+            stacks[to_stack-1].append(item)
+
+for stack in stacks:
+    result += stack[-1]
+
+print(result)
